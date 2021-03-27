9ebca1180d1802c8ba68971baf55eecb357d3d33
Muhammad Sohail Abbas <sohailabbas.100.10@gmail.com>
2021-03-27 18:22:22 +0500

    Complete

---
+++ app.js
@@ -0,0 +1,454 @@
+// Assignment # 9
+// JAVASCRIPT
+
+// ###############Objects of CHAPTER1###############
+//     ************ Q No. 1
+// alert("Welcome to my Website");
+// ************ Q No. 2
+// alert("Error! Please enter a valid password.");
+// ************ Q No. 3
+// alert("Welcome to JS Land...\n Happy Coding!");
+// ************ Q No. 4
+// alert("Welcome to JS Land...");
+// ************ Q No. 5
+// a) alert("Happy Coding");
+
+// ###############Objects of CHAPTER2###############
+//     ************ Q No. 1
+// var username;
+// var myName = "Muhammad Sohail Abbas";
+// ************ Q No. 3
+// var message;
+// var message = "Hello World Happy Coding";
+// alert(message);
+// ************ Q No. 4
+// var Stuname = "Bilal Hassan";
+// var age = 20;
+// var Qualification = "Undergraduate";
+// alert(Stuname);
+// alert(age + " Years Old");
+// alert(Qualification);
+// ************ Q No. 5
+// var Pizza = ("Pizza" + "<br />" + "Pizz" + "<br />" + "Piz" + "<br />" + "Pi" + "<br />" + "P");
+// document.write(Pizza);
+// ************ Q No. 6
+// var email = "sohail123@gmail.com";
+// alert(email)
+//     ************ Q No. 7
+// var book = "Smarter way to learn Java script is really a smarter way"
+// alert(book)
+//     ************ Q No. 8
+// var write = "Yay! I can write HTML content through Javascript";
+// alert(write)
+//     ************ Q No. 9
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(design)
+
+
+//     / ###############Objects of CHAPTER3###############
+
+//         ************ Q No. 1
+// var myAge = "My age is 19."
+// alert(myage);
+
+// ************ Q No. 2
+// var visit = "You have visited this website 09 times."
+// alert(visit);
+
+// ************ Q No. 3
+// var birthYear = "My birth year is 2001.\nData type of declared variable is string."
+// alert(birthYear);
+
+// ************ Q No. 4
+// var visitor = "Muhammad Sohail Abbas" + "<br>";
+// document.write(visitor);
+// var title = " black  T-shirt" + "<br>";
+// document.write(title)
+
+// var quantity = "03 T-shirts" + "<br>";
+// document.write(quantity)
+
+// var order = "Muhammad SohailAbbas ordered 03  black T-shirt on www.SABZAR.com"
+// document.write(order);
+
+// ###############Objects of CHAPTER4###############
+//     ************ Q No. 1
+// var variable1 = "Hello World!"
+// variable2 = true,
+//     variable3 = 42;
+
+// ************ Q No. 2
+// var hello = "";
+// var _hello = "";
+// var $hello = "";
+// var hello1 = "";
+// var myWorld = "";
+// var 143 = "";
+// var .hello = "";
+// var ?hello = "";
+// var @hello = "";
+// var & hello = "";
+
+// ###############Objects of CHAPTER6 - 9###############
+//     ************ Q No. 1
+// var user = 10;
+// document.write("Result :" + "<br/>" + "The value of user is :" + user + "<br/>" + "<hr/>");
+// ++user;
+// document.write("The value of ++user is :" + user + "<br/>" + "Now the value of user is :" + user + "<br/><br/>");
+// user++;
+// document.write("The value of user++ is :" + 11 + "<br/>" + "Now the value of user is :" + user + "<br/><br/>");
+// --user;
+// document.write("The value of --user is :" + user + "<br/>" + "Now the value of user is :" + user + "<br/><br/>");
+// user--;
+// document.write("The value of user-- is :" + 11 + "<br/>" + "Now the value of user is :" + user);
+// ************ Q No. 2
+// var a = 2;
+// var b = 1;
+
+// var result = --a - --b + ++b + b--;
+// Explain the output at each stage:
+// --a;
+// --a - --b;
+// --a - --b + ++b;
+// --a - --b + ++b + b--;
+// var a = 2;
+// var b = 1;
+// var result;
+// --a; (Now result = 1)
+// --a - --b; (Now result = 1)
+// --a - --b + ++b; (Now result = 2)
+// --a - --b + ++b + b--; (Now result = 3)
+// var result = --a - --b + ++b + b--;
+// document.write("a is 2" + "<br/>" + "b is 1" + "<br/>" + "result is " + result);
+
+// ************ Q No. 3
+// var greetName = prompt("Enter name");
+// alert("Welcome to our Webpage" + "\n" + greetName);
+// ************ Q No. 4
+// var user = prompt("Enter no for print table");
+// n = 1;
+// while (n <= 10, user <= user) {
+//     document.write(user + "x" + n + "=" + user * n + "<BR>")
+//     n++;
+//     if (n == 11) {
+//         d++;
+//         n = 0;
+//     }
+// }
+// ************ Q No. 6
+// var totalMarks = 100;
+// var sub1 = prompt("Enter Subject");
+// var sub2 = prompt("Enter Subject");
+// var sub3 = prompt("Enter Subject");
+// var obtained1 = prompt("Enter obtained Marks for sub 1");
+// var obtained2 = prompt("Enter obtained Marks for sub 2");
+// var obtained3 = prompt("Enter obtained Marks for sub 3");
+// var percent1 = (obtained1 / 100) * totalMarks;
+// var percent2 = (obtained2 / 100) * totalMarks;
+// var percent3 = (obtained3 / 100) * totalMarks;
+// document.write(" <b>Subject</b>" + " <b>Total</b>" + " <b>Obtained Marks</b>" + " <b>Pecentage</b> " + "<br/>");
+// document.write(sub1 + "  " + "  " + "  " + totalMarks + " " + obtained1 + " " + percent1 + "%" + "<br/>" + sub2 + " " + totalMarks + " " + obtained2 + " " + percent2 + "%" + "<br/>" + sub3 + " " + totalMarks + " " + obtained3 + " " + percent3 + "%");
+
+// ###############Objects of CHAPTER9 - 11###############
+//     ************ Q No. 1
+// var cityName = prompt("Enter city name");
+// if (cityName == "karachi") {
+//     document.write("Welcome to city of lights");
+// }
+// else if (cityName == "lahore") {
+//     document.write("Welcome to city of garden");
+// }
+// else {
+//     document.write("City of peace");
+// }
+// ************ Q No. 2
+// var gender = prompt("Enter gender");
+// if (gender == "male") {
+//     document.write("Good Morning Sir.");
+// }
+// else if (gender == "female") {
+//     document.write("Good Morning Ma’am.");
+// }
+// else {
+//     alert("please enter correct value");
+// }
+
+// ************ Q No. 3
+// var signal = prompt("Enter color for signal");
+// if (signal == "red") {
+//     alert("Must Stop don't violate it")
+// }
+// else if (signal == "yellow") {
+//     alert("Ready to move");
+// }
+// else if (signal == "green") {
+//     alert("Move now")
+// }
+// else {
+//     alert("Error");
+// }
+
+// ************ Q No. 4
+
+// var fuel = +prompt("Enter fuel limit in litres");
+// if (fuel < 0.25) {
+//     alert("Please refill the fuel in your car");
+// }
+// else {
+//     alert("No need to refill");
+// }    
+
+// ************ Q No. - 5
+// var a = 4;
+// if (++a === 5) { alert("given condition for variable a is true"); }//Condition is True
+// var b = 82;
+// if (b++ === 83) { alert("given condition for variable b is true"); }//Condition is False
+// var c = 12;
+// if (c++ === 13) { alert("condition 1 is true"); }
+// if (c === 13) { alert("condition 2 is true"); }//condition 2 & 4 is Y\True. 
+// if (++c < 14) { alert("condition 3 is true"); }
+// if (c === 14) { alert("condition 4 is true"); }  
+
+// ************ Q No. 6
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost) {
+//     alert("The cost equals"); //This condition is true. 
+// }
+// if (true) {
+//     alert("True");//this codition is True 
+// }
+// if (false) {
+//     alert("False");
+// }
+
+// if ("car" < "cat") {
+//     alert("car is smaller than cat"); //True 
+// }
+
+// ************ Q No. 7
+// var secretNum = 5;
+// var userNum = +prompt("Guess Secret Number 0-10 ");
+// if (userNum == 5) {
+//     alert("“Bingo! Correct answer”.");
+// }
+// else if (userNum == 6) {
+//     alert("“Close enough to the correct answer”.");
+// }
+// else {
+//     alert("Oops!Wrong Number");
+// }
+
+// ************ Q No. 8
+// var count = +prompt("Enter number");
+// if (count % 3 == 0) {
+//     alert("It is divisible by 3");
+// }
+// else {
+//     alert("Not divisible by 3");
+// }
+
+// ************ Q No. 9
+// var checkNum = +prompt("Enter num to check Even/Odd");
+// if (checkNum % 2 == 0) {
+//     alert("It's an Even Number")
+// }
+// else if (checkNum % 2 == 1) {
+//     alert("It's an Odd Number")
+// }
+// else {
+//     alert("Invalid number");
+// }
+
+// ************ Q No. 10
+// var temperature = +prompt("Enter Temperature value in degree Celcius");
+// if (temperature > 40) {
+//     alert("“It is too hot outside.” ")
+// }
+// else if (temperature > 30) {
+//     alert("“The Weather today is Normal.” ");
+// }
+// else if (temperature > 20) {
+//     alert("“Today’s Weather is cool.”");
+// }
+// else if (temperature > 10) {
+//     alert("“OMG! Today’s weather is so Cool.”");
+// }
+// else {
+//     alert("Out of range!");
+// }
+// ************ Q No. 11
+// var input1 = +prompt("Enter first no");
+// var input2 = +prompt("Enter second no");
+// var operand = prompt("Enter operand");
+// if (operand == "+") {
+//     document.write(input1 + input2);
+// }
+// if (operand == "-") {
+//     document.write(input1 - input2);
+// }
+// if (operand == "*") {
+//     document.write(input1 * input2);
+// }
+// if (operand == "/") {
+//     document.write(input1 / input2);
+// }
+
+// ###############Objects of CHAPTER12 - 13###############
+
+//     ************ Q No. 1
+
+// var character = prompt("Enter character");
+// var charCode = character.charCodeAt();
+// document.write("The ASCII value  of " + character + " = " + charCode + "<br/>");
+
+// if (charCode >= 65 && charCode <= 90) {
+//     document.write("\nYou have entered a capital letter");
+// }
+
+// else if (charCode >= 97 && charCode <= 122) {
+//     document.write("\nYou have entered a small letter");
+// }
+
+// else if (charCode >= 47 && charCode <= 57) {
+//     document.write("\nYou have entered a digit ");
+// }
+
+// else if (charCode >= 0 || charCode >= 47 || charCode >= 54 || charCode <= 64 || charCode >= 91 || charCode <= 96 || charCode >= 123 || charCode <= 127) {
+//     document.write("\nYou have entered a special character");
+// }
+
+// ************ Q No. 2
+
+// var num1 = +prompt("Enter First number");
+// var num2 = +prompt("Enter Second number");
+// if ((num1 >= 25) && (num1 <= 50) && (num2 >= 25 && num2 <= 50)) {
+//     if (num1 === num2) {
+//         document.write("Numbers are the same");
+//     }
+//     else if (num1 > num2) {
+//         document.write(num1);
+//     }
+//     else if (num2 > num1) {
+//         document.write(num2);
+//     }
+// }
+// else {
+//     document.write("Numbers Out of Range");
+// }
+
+// ************ Q No. 3
+// var number = prompt("Enter Number?")
+// if (number > 0) {
+//     document.write(number + " is a positive number");
+// }
+// else if (number < 0) {
+//     document.write(number + " is a negative number");
+// }
+// else {
+//     document.write(number + " is neither positive nor negative");
+// }
+// ************ Q No. 4
+// var vowel = prompt("Enter Character to check vowel?");
+// vowel1 = vowel.toLowerCase();
+// if (vowel1 == "a" || vowel1 == "e" || vowel1 == "i" || vowel1 == "o" || vowel1 == "u") {
+//     alert(vowel1 + " is a vowel");
+// }
+// else {
+//     alert("Not a vowel");
+// }
+// ************ Q No. 5
+// var storePassword = prompt('Set password');
+// var askPassword = prompt("Enter your password");
+// if (storePassword == askPassword) {
+//     alert("“Correct! The password you entered matches the original password”.");
+// }
+// else {
+//     alert("“Incorrect password”");
+// }
+// ************ Q No. 6
+// var hour = 13;
+// if (hour < 18) {//adiing { 
+//     greeting = "Good day";
+//     alert(greeting);//addition
+// }//adding }
+// else {
+//     greeting = "Good evening";
+//     alert(greeting);
+// }
+
+// ************ Q No. 7
+
+// var time = prompt("Enter hours");
+// if (time == 1900) {
+//     alert("Its 7pm");
+// }
+// else if (time >= 0000 && time < 1200) {
+//     alert("Good Morning");
+// }
+// else if (time >= 1200 && time < 1700) {
+//     alert("Good Afternoon");
+// }
+// else if (time >= 1700 && time < 2100) {
+//     alert("Good Evening");
+// }
+// else if (time >= 2100 && time < 2359) {
+//     alert("Good Night");
+// }
+// else {
+//     alert("Invalid Input");
+// }
+
+// ###############Objects of CHAPTER14 - 16###############
+
+//     ************ Q No. 1
+// var studentNames = [];
+// ************ Q No. 2
+// var studentsNane = newArray({});
+// ************ Q No. 3
+// var string = ["SABZAR"];
+// ************ Q No. 4
+// var number = [71, 92, 35, 68, 4, 75, 47, 84, 98, 60];
+// ************ Q No. 5
+// var boolean = [true];
+// ************ Q No. 6
+// var mixArr = ["SABZAR", 2021, true];
+// ************ Q No. 7
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M.Phil", "PhD"];
+// document.write('<h2>' + "Qualifications :" + '</h2>' + '<ol><li>' + qualifications.join('</li><li>') + '</li></ol>');
+
+// ************ Q No. 8
+// var colorNames = ["red", 'pink', 'yellow', 'green', 'black', 'blue', 'brown', 'purple'];
+// a.Ask the user what color he / she wants to add to the beginning & add that color to the beginning of the array.Display the updated array in your browser.
+// var addColor = prompt("Enter color name you wants to add in start");
+// colorNames.unshift(addColor);
+// b.Ask the user what color he / she wants to add to the end & add that color to the end of the array.
+// Display the updated array in your browser.
+// var addColor = prompt("Enter color name you wants to add in last");
+// colorNames.push(addColor);
+// c.Add two more color to the beginning of the array.
+// Display the updated array in your browser.
+//     colorNames.unshift("white", "grey");
+// d.Delete the first color in the array.Display the updated array in your browser.
+//     colorNames.shift();
+// e.Delete the last color in the array.Display the updated array in your browser.
+//     colorNames.pop();
+// f.Ask the user at which index he / she wants to add a color & color name.
+// Then add the color to desired position / index.
+// Display the updated array in your browser.
+// var addColor = prompt("Enter color name you wants to add in desire index");
+// colorNames.splice(3, 0, "lightblue");
+// g.Ask the user at which index he / she wants to delete color(s) & how many colors he / she wants to delete.
+// Then remove the same number of color(s) from user - defined position / index.
+// Display the updated array in your browser.
+// var addColor = prompt("Enter color name you wants to delete in desire index");
+// colorNames.splice(3, 2, 0);
+// ************ Q No. 10
+// var number = [320, 333, 345, 111]
+// number.sort()
+// document.write(number);
+//     ************ Q No. 11
+// var city = ["ORMARA", "GAWADAR", "CHAKWAL", "Quetta"]
+// document.write(city[2], ",", city[3])
+
